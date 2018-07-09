824b07c00dae1bcb3d29299f50dac48c912f9361
limteng-rpi <liny9@rpi.edu>
2018-07-09 16:11:20 -0400

    evaluate mono-lingual single task model

--- train_single.py
+++ train_single.py
@@ -1,17 +1,23 @@
+import os
+import time
+import traceback
+
+import tqdm
+
 from torch import optim
 from torch.nn.utils import clip_grad_norm
 
 import constant as C
 from model import Linear, LSTM, CRF, CharCNN, Highway, LstmCrf, Embedding
 from argparse import ArgumentParser
-from util import get_logger, Config
+from util import get_logger, evaluate, Config
 from data import (
     SequenceDataset, ConllParser,
     compute_metadata, count2vocab, numberize_datasets
 )
 from task import NameTagging, MultiTask
 
-logger = get_logger(__name__)
+timestamp = time.strftime('%Y%m%d_%H%M%S', time.gmtime())
 
 
 argparser = ArgumentParser()
@@ -19,6 +25,7 @@ argparser = ArgumentParser()
 argparser.add_argument('--train', help='Path to the training set file')
 argparser.add_argument('--dev', help='Path to the dev set file')
 argparser.add_argument('--test', help='Path to the test set file')
+argparser.add_argument('--log', help='Path to the log dir')
 argparser.add_argument('--batch_size', default=10, type=int, help='Batch size')
 argparser.add_argument('--max_epoch', default=100, type=int)
 argparser.add_argument('--embedding', help='Path to the pre-trained embedding file')
@@ -26,6 +33,8 @@ argparser.add_argument('--embed_skip_first', default=True,
                        help='Skip the first line of the embedding file')
 argparser.add_argument('--word_embed_dim', type=int, default=100,
                        help='Word embedding dimension')
+argparser.add_argument('--word_embed_case', type=int, default=0,
+                       help='Word embedding case-sensitive')
 argparser.add_argument('--char_embed_dim', type=int, default=50,
                        help='Character embedding dimension')
 argparser.add_argument('--charcnn_filters', default='2,25;3,25;4,25',
@@ -54,29 +63,47 @@ embed_file = args.embedding
 charcnn_filters = [[int(f.split(',')[0]), int(f.split(',')[1])]
                    for f in args.charcnn_filters.split(';')]
 use_gpu = (args.gpu == 1)
+word_ignore_case = (args.word_embed_case == 0)
+log_writer = None
+if args.log:
+    log_file = os.path.join(args.log, 'log.{}.txt'.format(timestamp))
+    log_writer = open(log_file, 'a', encoding='utf-8')
+    logger = get_logger(__name__, log_file=log_file)
+    # logger.info('Log file: {}'.format(log_file))
+else:
+    logger = get_logger(__name__)
+
+logger.info('----------')
+logger.info('Parameters:')
+for arg in vars(args):
+    logger.info('{}: {}'.format(arg, getattr(args, arg)))
+logger.info('----------')
 
 # Parser for CoNLL format file
 conll_parser = ConllParser(Config({
     'separator': ' ',
     'token_col': 0,
     'label_col': 3,
-    'comment': False,
+    'skip_comment': False,
 }))
 
 # Load datasets
+logger.info('Loading datasets')
 train_set = SequenceDataset(Config({
     'path': args.train, 'parser': conll_parser, 'batch_size': args.batch_size}))
 dev_set = SequenceDataset(Config({
     'path': args.dev, 'parser': conll_parser}))
 test_set = SequenceDataset(Config({
     'path': args.test, 'parser': conll_parser}))
+datasets = {'train': train_set, 'dev': dev_set, 'test': test_set}
 
 # Vocabs
+logger.info('Building vocabularies')
 token_count, label_count, char_count = compute_metadata(
     [train_set, dev_set, test_set])
 token_vocab = count2vocab([token_count],
                           start_idx=C.EMBED_START_IDX,
-                          ignore_case=True)
+                          ignore_case=word_ignore_case)
 label_vocab = count2vocab([label_count],
                           start_idx=0,
                           sort=True,
@@ -85,6 +112,7 @@ char_vocab = count2vocab([char_count],
                          ignore_case=False,
                          start_idx=C.CHAR_EMBED_START_IDX)
 if embed_file:
+    logger.info('Scaning pre-trained embeddings')
     with open(embed_file, 'r', encoding='utf-8') as embed_r:
         if args.embed_skip_first:
             embed_r.readline()
@@ -96,20 +124,25 @@ if embed_file:
                     token_vocab[token] = len(token_vocab) + C.EMBED_START_IDX
             except UnicodeDecodeError as e:
                 logger.warning(e)
+idx_token = {idx: token for token, idx in token_vocab.items()}
+idx_label = {idx: label for label, idx in label_vocab.items()}
+idx_token[C.UNKNOWN_TOKEN_INDEX] = C.UNKNOWN_TOKEN
 
 # Numberize datasets
+logger.info('Numberizing datasets')
 numberize_datasets(
     [
         (train_set, token_vocab, label_vocab, char_vocab),
         (dev_set, token_vocab, label_vocab, char_vocab),
         (test_set, token_vocab, label_vocab, char_vocab),
     ],
-    token_ignore_case=True,
+    token_ignore_case=word_ignore_case,
     label_ignore_case=False,
     char_ignore_case=False
 )
 
 # Model components
+logger.info('Building the model')
 word_embed = Embedding(Config({
     'num_embeddings': len(token_vocab),
     'embedding_dim': args.word_embed_dim,
@@ -119,7 +152,8 @@ word_embed = Embedding(Config({
     'trainable': True,
     'file': embed_file,
     'stats': args.embed_skip_first,
-    'vocab': token_vocab
+    'vocab': token_vocab,
+    'ignore_case': word_ignore_case
 }))
 char_cnn = CharCNN(Config({
     'vocab_size': len(char_vocab),
@@ -163,33 +197,71 @@ lstm_crf = LstmCrf(
     char_highway=char_highway
 )
 
-# Task
+if use_gpu:
+    lstm_crf.cuda()
 
+# Task
 optimizer = optim.SGD(filter(lambda p: p.requires_grad, lstm_crf.parameters()),
                       lr=args.lr, momentum=args.momentum)
 
-global_step = 0
-for epoch in range(args.max_epoch):
-    for batch in train_set.get_dataset(
-            gpu=use_gpu, shuffle_inst=True, batch_size=args.batch_size):
-        global_step += 1
-        optimizer.zero_grad()
-        tokens, labels, chars, seq_lens, char_lens = batch
-        loglik, _ = lstm_crf.loglik(tokens, labels, seq_lens, chars, char_lens)
-        loss = -loglik.mean()
-        loss.backward()
+try:
+    global_step = 0
+    best_dev_score = 0.0
 
-        params = []
-        for n, p in lstm_crf.named_parameters():
-            if 'embedding.weight' not in n:
-                params.append(p)
-        clip_grad_norm(params, args.grad_clipping)
-        optimizer.step()
+    for epoch in range(args.max_epoch):
+        logger.info('Epoch {}: Training'.format(epoch))
 
-    # TODO: evaluate the model
-    # TODO: save the best model
+        for ds in ['train', 'dev', 'test']:
+            dataset = datasets[ds]
+            epoch_loss = []
+            results = []
 
-    # learning rate decay
-    lr = args.lr * args.decay_rate ** (global_step / args.decay_step)
-    for p in optimizer.param_groups:
-        p['lr'] = lr
+            progress = tqdm.tqdm(total=dataset.batch_num(args.batch_size), mininterval=1, desc=ds)
+            for batch in dataset.get_dataset(
+                    gpu=use_gpu,
+                    shuffle_inst=ds=='train',
+                    batch_size=args.batch_size):
+                global_step += 1
+                optimizer.zero_grad()
+                progress.update(1)
+                tokens, labels, chars, seq_lens, char_lens = batch
+                if ds == 'train':
+                    lstm_crf.train()
+                    loglik, _ = lstm_crf.loglik(
+                        tokens, labels, seq_lens, chars, char_lens)
+                    loss = -loglik.mean()
+                    loss.backward()
+
+                    params = [p for n, p in lstm_crf.named_parameters()
+                              if 'embedding.weight' not in n]
+                    clip_grad_norm(params, args.grad_clipping)
+                    optimizer.step()
+
+                else:
+                    lstm_crf.eval()
+                    pred, loss = lstm_crf.predict(
+                        tokens, labels, seq_lens, chars, char_lens)
+                    results.append((pred, labels, seq_lens, tokens))
+
+                epoch_loss.append(loss.data[0])
+            progress.close()
+
+            epoch_loss = sum(epoch_loss) / len(epoch_loss)
+            logger.info('{} Loss: {:.4f}'.format(ds, epoch_loss))
+
+            if ds == 'dev' or ds == 'test':
+                fscore, prec, rec = evaluate(
+                    results, idx_token, idx_label, writer=log_writer)
+                if ds == 'dev' and fscore > best_dev_score:
+                    best_dev_score = fscore
+                    # TODO: save the best model
+
+        # learning rate decay
+        lr = args.lr * args.decay_rate ** (global_step / args.decay_step)
+        for p in optimizer.param_groups:
+            p['lr'] = lr
+        logger.info('New learning rate: {}'.format(lr))
+except KeyboardInterrupt:
+    traceback.print_exc()
+    if log_writer:
+        log_writer.close()
