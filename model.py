efd37a182eabaede0ef2f83458e414bcd5371fe0
limteng-rpi <liny9@rpi.edu>
2018-08-09 11:15:42 -0400

    update highway network implementation

--- model.py
+++ model.py
@@ -91,9 +91,9 @@ class Highway(nn.Module):
         self.nonlinear = nn.ModuleList([
             Linear(Config({'in_features': size, 'out_features': size}))
             for _ in range(num_layers)])
-        self.linear = nn.ModuleList([
-            Linear(Config({'in_features': size, 'out_features': size}))
-            for _ in range(num_layers)])
+        # self.linear = nn.ModuleList([
+        #     Linear(Config({'in_features': size, 'out_features': size}))
+        #     for _ in range(num_layers)])
         self.gate = nn.ModuleList([
             Linear(Config({'in_features': size, 'out_features': size}))
             for _ in range(num_layers)])
@@ -102,19 +102,19 @@ class Highway(nn.Module):
         """
         :param x: tensor with shape of [batch_size, size]
         :return: tensor with shape of [batch_size, size]
-        applies σ(x) ⨀ (f(G(x))) + (1 - σ(x)) ⨀ (Q(x)) transformation | G and
-         Q is affine transformation, f is non-linear transformation, σ(x) is
-         affine transformation with sigmoid non-linearition and ⨀ is element-
-         wise multiplication
+        See: https://github.com/kefirski/pytorch_Highway/blob/master/highway/highway.py
         """
 
         for layer in range(self.num_layers):
             gate = F.sigmoid(self.gate[layer](x))
 
             nonlinear = self.f(self.nonlinear[layer](x))
-            linear = self.linear[layer](x)
 
-            x = gate * nonlinear + (1 - gate) * linear
+            # Remove the affine transformation
+            # linear = self.linear[layer](x)
+            # x = gate * nonlinear + (1 - gate) * linear
+
+            x = gate * nonlinear + (1 - gate) * x
 
         return x
 
